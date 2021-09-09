aae4872e9196c800cee40718549105647809b102
Arvianto Dwi <arvi@ata.network>
2021-09-09 16:28:35 +0700

    Test relay URI entries

--- src/hooks/useVercelEnvironment.ts
+++ src/hooks/useVercelEnvironment.ts
@@ -1,6 +1,7 @@
 const useVercelEnvironment = () => {
   const isProduction = process.env.DEPLOYMENT_ENV && process.env.DEPLOYMENT_ENV === 'production'
-  const deploymentEnv = process.env.DEPLOYMENT_ENV
+  const deploymentEnv = process.env.DEPLOYMENT_ENV || 'staging'
+  console.log('deploymentEnv', deploymentEnv)
   return { isProduction, deploymentEnv }
 }
 
