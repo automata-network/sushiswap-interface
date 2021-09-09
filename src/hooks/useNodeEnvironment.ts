d593c4eee274af901f912980499692e1fe31f4fc
Arvianto Dwi <arvi@ata.network>
2021-09-09 16:58:27 +0700

    Use staging until Geode ready for production

--- src/hooks/useNodeEnvironment.ts
+++ src/hooks/useNodeEnvironment.ts
@@ -2,7 +2,8 @@ const useVercelEnvironment = () => {
   const isProduction = process.env.NODE_ENV && process.env.NODE_ENV === 'production'
   const deploymentEnv = process.env.NODE_ENV && process.env.NODE_ENV !== 'production' ? 'staging' : 'production'
 
-  return { isProduction, deploymentEnv }
+  // TODO Remove 'staging' for deploymentEnv value when production version of Geode is ready
+  return { isProduction, deploymentEnv: 'staging' }
 }
 
 export default useVercelEnvironment
